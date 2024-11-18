a362c616fb1f159421c2e207435478ed6e08bca5
SantiaGOAT14 <136783629+SantiaGOAT14@users.noreply.github.com>
2024-11-18 11:00:24 -0600

    Ejercicio3.c

---
+++ Ejercicio3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    float dolares, pesos;
+    const float tasaCambio = 11.96;
+
+    printf("Ingrese la cantidad en dólares: ");
+    scanf("%f", &dolares);
+
+    pesos = dolares * tasaCambio;
+
+    printf("La cantidad en pesos es: %.2f\n", pesos);
+
+    return 0;
+}
